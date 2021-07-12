d39e2d446ae13e31d3a0148ec32f68bf0a2de6a9
prussianman <46068652+prussianman@users.noreply.github.com>
2021-07-12 23:33:54 +0800

    Changes again

--- db/indiv db/awsContributionHIstory.sql
+++ db/indiv db/awsContributionHIstory.sql
@@ -4,11 +4,13 @@ Use awsContributionHistory;
 
 CREATE TABLE awsContributionHistory(
 volunteer_id INT NOT NULL,
-initiative_id INT NOT NULL,
+event_id INT NOT NULL,
 resource_name VARCHAR(255) NOT NULL,
 resource_qty INT NOT NULL,
 
 -- Do we need this? (To record date and time of review)
 date DATE NOT NULL,
 time TIME NOT NULL
+
+PRIMARY KEY (volunteer_id, event_id, resource_name)
 );
